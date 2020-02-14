451fbf4cc08a5655c006052370b13bc4fda76f56
tsitsakosa <60984763+tsitsakosa@users.noreply.github.com>
2020-02-14 23:38:33 +0100

    Fetch latest articles ids

--- src/components/NewsFeed/NewsFeed.js
+++ src/components/NewsFeed/NewsFeed.js
@@ -1,24 +1,43 @@
 import React, { Component } from 'react';
-import { Row } from 'react-bootstrap';
+import { Row, Alert } from 'react-bootstrap';
 import Article from './Article';
 import axios from '../../axiosRequests';
 
 
 class NewsFeed extends Component {
     state = {
+        latestArticles: [],
+        error: false
+    }
 
+    componentDidMount() {
+        axios.get('/v0/topstories.json')
+            .then(response => {
+                const articles = response.data.slice(0, 6);
+
+                this.setState({ latestArticles: articles });
+                console.log(this.state.latestArticles);
+            })
+            .catch(error => {
+                // console.log(error);
+                this.setState({ error: true });
+            });
     }
 
     render() {
+        let articles = <Alert variant="danger">Something went wrong! Try later..</Alert>;
+        if (!this.state.error) {
+            articles = this.state.latestArticles.map(article => {
+                return <Article
+                    key={article.id}
+                    id={article.id} />;
+            });
+            articles = <Row>{articles}</Row>
+        }
         return (
-            <Row>
-               <Article/>
-               <Article/>
-               <Article/>
-               <Article/>
-               <Article/>
-               <Article/>
-            </Row>
+            <React.Fragment>
+                {articles}
+            </React.Fragment>
         );
     }
 
