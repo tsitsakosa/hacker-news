cd892c433aab991d861949c3b14b0428b66b0478
tsitsakosa <60984763+tsitsakosa@users.noreply.github.com>
2020-02-15 01:26:31 +0100

    Fetch current page data and bind to the view

--- src/components/NewsFeed/Article.js
+++ src/components/NewsFeed/Article.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 import { Col, Card, Button } from 'react-bootstrap';
 
 const article = (props) => (
-    <Col xs={12} md={6} lg={4}>
+    <Col xs={12}>
         <Card className="mb-4">
-            <Card.Header>Featured</Card.Header>
             <Card.Body>
-                <Card.Title>Special title treatment</Card.Title>
+                <Card.Title><h2>{props.title}</h2></Card.Title>
                 <Card.Text>With supporting text below as a natural lead-in to additional content.</Card.Text>
                 <Button variant="primary">Go somewhere</Button>
             </Card.Body>
--- src/components/NewsFeed/NewsFeed.js
+++ src/components/NewsFeed/NewsFeed.js
@@ -6,31 +6,64 @@ import axios from '../../axiosRequests';
 
 class NewsFeed extends Component {
     state = {
+        latestArticlesIds: [],
         latestArticles: [],
+        currentPage: 1,
+        currentArticles: [],
         error: false
     }
 
-    componentDidMount() {
+    fetchLatestArticlesIds = () => {
         axios.get('/v0/topstories.json')
             .then(response => {
                 const articles = response.data.slice(0, 6);
-
-                this.setState({ latestArticles: articles });
-                console.log(this.state.latestArticles);
+                this.setState({ latestArticlesIds: articles });
+                console.debug(this.state.latestArticlesIds);
+                this.fetchLatestArticles();
             })
             .catch(error => {
-                // console.log(error);
+                console.debug("fetchLatestArticlesIds :", error);
                 this.setState({ error: true });
             });
+    }
+
+    fetchLatestArticles = () => {
+        const offset = 7;
+        const currentPage = this.state.currentPage;
+        const firstItem = currentPage * offset - offset;
+        const lastItem = currentPage * offset - 1;
+        const currentPageArticlesIds = this.state.latestArticlesIds.slice(firstItem, lastItem);
+        const currentArticles = [];
+        let promises = [];
+
+        currentPageArticlesIds.forEach(id => {
+            console.debug(id);
+            promises.push(axios.get('/v0/item/' + id + '.json'));
+        });
+
+        Promise.all(promises).then(results => {
+            results.forEach(response => {
+                const data = response.data;
+                currentArticles.push(data);
+                console.debug(response.data);
+            });
+            this.setState({ currentArticles: currentArticles });
+        });
+    }
+
+    componentDidMount() {
+        this.fetchLatestArticlesIds();
     }
 
     render() {
         let articles = <Alert variant="danger">Something went wrong! Try later..</Alert>;
         if (!this.state.error) {
-            articles = this.state.latestArticles.map(article => {
+            articles = this.state.currentArticles.map(article => {
                 return <Article
                     key={article.id}
-                    id={article.id} />;
+                    id={article.id}
+                    title={article.title}
+                     />;
             });
             articles = <Row>{articles}</Row>
         }
